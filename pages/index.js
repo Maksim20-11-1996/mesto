a034af9006492cfbbd0db6b5c949ed53aba64988
Maksim Naumov <maksim-naumov.1996@mail.ru>
2021-06-17 18:09:10 +0300

    first version

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ const PopupOpenButtonElement = document.querySelector('.profile__edit-button');
 const PopupElement = document.querySelector('.popup');
 const PopupCloseButtonElement = PopupElement.querySelector('.popup__close-button');
 const PopupFormElement = document.querySelector('.popup__form');
-const PopupInputTitleElement = PopupElement.querySelector('.popup__form_title');
-const PopupInputSubtitleElement = PopupElement.querySelector('.popup__form_subtitle');
+const PopupInputTitleElement = PopupElement.querySelector('.popup__form-title');
+const PopupInputSubtitleElement = PopupElement.querySelector('.popup__form-subtitle');
 const ProfileInfoSaveButton = PopupElement.querySelector('.popup__save-button');
 
 const OpenPopup = function () {
