1273988fc759b4ff69659194fb3ca1c0c1b6bec8
Maksim Naumov <maksim-naumov.1996@mail.ru>
2021-06-19 18:45:31 +0300

    fixed version

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,36 @@
-const ProfileInfoTitle = document.querySelector('.profile__info-title');
-const ProfileInfoSubtitle = document.querySelector('.profile__info-subtitle');
-const PopupOpenButtonElement = document.querySelector('.profile__edit-button');
-const PopupElement = document.querySelector('.popup');
-const PopupCloseButtonElement = PopupElement.querySelector('.popup__close-button');
-const PopupFormElement = document.querySelector('.popup__form');
-const PopupInputTitleElement = PopupElement.querySelector('.popup__form-title');
-const PopupInputSubtitleElement = PopupElement.querySelector('.popup__form-subtitle');
-const ProfileInfoSaveButton = PopupElement.querySelector('.popup__save-button');
+const profileInfoTitle = document.querySelector('.profile__info-title');
+const profileInfoSubtitle = document.querySelector('.profile__info-subtitle');
+const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
+const popupFormElement = document.querySelector('.popup__form');
+const popupInputTitleElement = popupElement.querySelector('.popup__profile_name');
+const popupInputSubtitleElement = popupElement.querySelector('.popup__profile_job');
 
-const OpenPopup = function () {
-    PopupInputTitleElement.value = ProfileInfoTitle.textContent;
-    PopupInputSubtitleElement.value = ProfileInfoSubtitle.textContent;
-    PopupElement.classList.add('popup_is-opened');
+const openPopup = function() {
+    popupInputTitleElement.value = profileInfoTitle.textContent;
+    popupInputSubtitleElement.value = profileInfoSubtitle.textContent;
+    popupElement.classList.add('popup_is-opened');
 }
 
-const ClosePopup = function () {
-    PopupElement.classList.remove('popup_is-opened');
+const closePopup = function() {
+    popupElement.classList.remove('popup_is-opened');
 }
 
-const ClosePopupByClickOverlay = function(event) {
-    if (event.target !== event.currentTarget) {
-        return
-    }
-    ClosePopup()
-}
+// const closePopupByClickOverlay = function(event) {
+//     if (event.target !== event.currentTarget) {
+//         return
+//     }
+//     closePopup()
+// }
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
     evt.preventDefault();
-    ProfileInfoTitle.textContent = PopupInputTitleElement.value
-    ProfileInfoSubtitle.textContent = PopupInputSubtitleElement.value
-    ClosePopup()
+    profileInfoTitle.textContent = popupInputTitleElement.value
+    profileInfoSubtitle.textContent = popupInputSubtitleElement.value
+    closePopup()
 }
 
-PopupOpenButtonElement.addEventListener('click', OpenPopup);
-PopupCloseButtonElement.addEventListener('click', ClosePopup);
-PopupElement.addEventListener('click', ClosePopupByClickOverlay);
-PopupFormElement.addEventListener('submit', formSubmitHandler);
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+popupFormElement.addEventListener('submit', formSubmitHandler);
